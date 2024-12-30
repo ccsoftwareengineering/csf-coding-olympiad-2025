dbfc29b079fbde7db64aa02819fda031cf21405f
Upsidedly <matthewlloydw@gmail.com>
2024-12-30 18:25:12 -0500

    fix: uncomment rest of dialogue

--- modules/dialogue.py
+++ modules/dialogue.py
@@ -10,9 +10,9 @@ if typing.TYPE_CHECKING:
 def dialogues(game: 'Game'):
     return {
         'introduction': ([
-            # ("Welcome to Power Island!", {}),
-            # ("In this game, you are tasked with managing a country's energy resources.", {}),
-            # ("Before you start playing, we want to know a few things...", {}),
+            ("Welcome to Power Island!", {}),
+            ("In this game, you are tasked with managing a country's energy resources.", {}),
+            ("Before you start playing, we want to know a few things...", {}),
             ("What is your name? ", {
                 "input": input_data("name", placeholder="Write your name here", options={"min_length": 3}),
                 "input_submit": lambda data: setattr(game, 'player', Player(data)),
