7bb93cff78c51a5dceefe8519516fce2d4d50368
Upsidedly <matthewlloydw@gmail.com>
2025-01-05 02:42:39 -0500

    feat: add new cursor type

--- structures/handlers/cursor_handler.py
+++ structures/handlers/cursor_handler.py
@@ -36,6 +36,7 @@ class CursorHandler:
             'HIGHLIGHT': self.get_cursor_and_offsets((6, 17), (-5, 0)),
             'INPUT': self.get_cursor_and_offsets((7, 1)),
             'NEXT': self.get_cursor_and_offsets((3, 12)),
+            'ADD': self.get_cursor_and_offsets((6, 2), (-8, 0)),
         }
         self._cursor = 'NORMAL'
         self.cursor_icon = self.cursors['NORMAL'][0]
@@ -47,15 +48,20 @@ class CursorHandler:
 
     @cursor.setter
     def cursor(self, cursor: str | tuple[int, int]):
+        if self._cursor == cursor:
+            return
         _cursor = self.cursors.get(cursor)
         if _cursor:
             self.cursor_icon = _cursor[0]
             self.cursor_offset = _cursor[1]
+            self._cursor = cursor
         elif type(cursor) is tuple:
             self.cursor_icon = self.get_cursor_icon(cursor)
             self.cursor_offset = (0, 0)
+            self._cursor = cursor
         else:
             self.cursor_icon, self.cursor_offset = self.cursors.get('NORMAL')
+            self._cursor = 'NORMAL'
 
     def draw(self):
         pos = pygame.mouse.get_pos()
