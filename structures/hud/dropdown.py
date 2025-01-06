e6c04d86e0d81928e202a401093d583af22e815f
Upsidedly <matthewlloydw@gmail.com>
2025-01-06 13:47:52 -0500

    feat: add dropdown for button

---
+++ structures/hud/dropdown.py
@@ -0,0 +1,65 @@
+from typing import TYPE_CHECKING
+
+import modules.utilities as u
+from structures.hud.button import Button
+from structures.hud.dynamic_hud_object import DynamicHudObject
+
+if TYPE_CHECKING:
+    from structures.game import Game
+
+
+class Dropdown(DynamicHudObject):
+    def __init__(
+            self,
+            game: 'Game',
+            size: tuple[int, int],
+            button: Button,
+            gap: int = 20,
+            rect_template: u.RectTemplate = None,
+            pos: (int, int) = (0, 0),
+            scale: float = 1,
+            parent=None,
+            id=None,
+            children_enabled=True,
+            **kwargs
+    ):
+        self.gap = gap
+        self.button = button
+        self.button.subscribe('on_press_end', self.on_button_press)
+        self._selected = False
+        super().__init__(game, size, rect_template, pos, scale, parent, id, children_enabled)
+        self.visible = False
+
+    @property
+    def selected(self):
+        return self._selected
+
+    def on_button_press(self, button):
+        self.selected = self.button.enabled
+
+    @selected.setter
+    def selected(self, selected: bool):
+        if self._selected == selected:
+            return
+        self._selected = selected
+        if selected:
+            self.game.input_handler.subscribe("mouse_on_up", self.on_mouse_up, "dropdown_selection")
+            self.visible = True
+        else:
+            self.visible = False
+            self.game.input_handler.unsubscribe("mouse_on_up", "dropdown_selection")
+            pass
+
+    def on_mouse_up(self, event):
+        if event.button != 1:
+            return
+        self.selected = self.hovering and self.enabled
+
+    def predraw(self):
+        self.game.telemetry_handler.set_value('add button', (self.button.hovering, self.button.on_hover_start,
+                                              self.button.on_hover_end, self.button.on_press_start,
+                                              self.button.on_press_end))
+        # if self.button.on_press_end and self.button.enabled:
+        #     self.selected = True
+        self.rect.midtop = (self.button.absolute_rect.centerx, self.button.absolute_rect.bottom + self.gap)
+        super().predraw()
---
+++ structures/hud/dynamic_hud_object.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING
+
+import pygame
+from pygame import Surface
+
+import modules.utilities as u
+
+from structures.hud.hud_object import HudObject
+
+if TYPE_CHECKING:
+    from structures.game import Game
+
+
+class DynamicHudObject(HudObject):
+    def __init__(
+            self,
+            game: 'Game',
+            size: tuple[int, int],
+            rect_template: u.RectTemplate = None,
+            position: (int, int) = (0, 0),
+            scale: float = 1,
+            parent=None,
+            id=None,
+            children_enabled=True,
+            **kwargs
+    ):
+        self.rect_template = rect_template
+        self._size = size
+        surf = Surface(size, pygame.SRCALPHA)
+        if rect_template:
+            surf.blit(rect_template(size), (0, 0))
+        super().__init__(game, surf, position, scale, parent, id, children_enabled)
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value: tuple[int, int]):
+        self._size = value
+        self.rect.size = value
+        surf = Surface(value, pygame.SRCALPHA)
+        if self.rect_template:
+            surf.blit(self.rect_template(value), (0, 0))
